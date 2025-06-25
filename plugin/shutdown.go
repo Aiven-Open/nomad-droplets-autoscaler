c9d1b10734ab3b9002ca6ada8a7764ac901c45c7
Nick Farrell <nick.farrell@aiven.io>
2025-06-26 07:21:27 +1000

    Provide a DigitalOcean wrapper
    the godo package is great, but makes it difficult to partially mock out.
    By defining an interfaces which provides methods to access the subsets
    of the godo package which is being used, it becomes possible to write
    unit tests on code which interacts with DigitalOcean

--- plugin/shutdown.go
+++ plugin/shutdown.go
@@ -5,32 +5,32 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/digitalocean/godo"
 	"github.com/hashicorp/go-hclog"
 )
 
 func shutdownDroplet(
 	ctx context.Context,
 	dropletId int,
-	client *godo.Client,
+	droplets Droplets,
+	dropletActions DropletActions,
 	log hclog.Logger,
 ) error {
 	// Gracefully power off the droplet.
 	log.Debug("Gracefully shutting down droplet...")
-	_, _, err := client.DropletActions.PowerOff(ctx, dropletId)
+	_, _, err := dropletActions.PowerOff(ctx, dropletId)
 	if err != nil {
 		return fmt.Errorf("error shutting down droplet: %w", err)
 	}
 
 	ctxWaitForDropletState, cancel := context.WithTimeout(ctx, 5*time.Minute)
 	defer cancel()
-	err = waitForDropletState(ctxWaitForDropletState, "off", dropletId, client, log)
+	err = waitForDropletState(ctxWaitForDropletState, "off", dropletId, droplets, log)
 	if err != nil {
 		log.Warn("Timeout while waiting to for droplet to become 'off'", "error", err)
 	}
 
 	log.Debug("Deleting Droplet...")
-	_, err = client.Droplets.Delete(ctx, dropletId)
+	_, err = droplets.Delete(ctx, dropletId)
 	if err != nil {
 		return fmt.Errorf("error deleting droplet: %w", err)
 	}
